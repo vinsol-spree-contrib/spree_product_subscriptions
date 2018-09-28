dc73a6ec2e96f9dcc0dce522c73f8500881eb8f3
wkosak <lamkosak@gmail.com>
2018-09-28 12:19:03 +0200

    TM-159 variant_id not required, view woo_id attribute instead of Product

--- app/models/spree/subscription.rb
+++ app/models/spree/subscription.rb
@@ -42,10 +42,9 @@ module Spree
       validates :price, numericality: { greater_than_or_equal_to: 0 }
       validates :quantity, numericality: { greater_than: 0, only_integer: true }
       validates :delivery_number, numericality: { greater_than_or_equal_to: :recurring_orders_size, only_integer: true }
-      validates :parent_order, uniqueness: { scope: :variant }
     end
     with_options presence: true do
-      validates :quantity, :delivery_number, :price, :number, :variant, :parent_order, :frequency
+      validates :quantity, :delivery_number, :price, :number, :parent_order, :frequency
       validates :cancellation_reasons, :cancelled_at, if: :cancelled
       validates :ship_address, :bill_address, :next_occurrence_at, :source, if: :enabled?
     end
