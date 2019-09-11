35812573262b6761438fa7788cec74f09165c9b7
Adam Foldvari <foldadam93@gmail.com>
2019-09-11 15:03:50 +0200

    Delete product page frontend

--- app/overrides/add_checkboxes_to_product_show_page.rb
+++
@@ -1,6 +0,0 @@
-Deface::Override.new(
-  virtual_path: "spree/products/_cart_form",
-  name: "add_checkboxes_to_cart_form",
-  insert_before: ".add-to-cart",
-  partial: "spree/products/cart_checkboxes"
-)
