60d78c902abe32e6f9378c5c68b31af89db2e17a
Kris Birov <kris.birov@gmail.com>
2019-11-28 14:59:16 +0100

    remove Spree::Label reference

--- app/models/spree/subscription_label_status.rb
+++ app/models/spree/subscription_label_status.rb
@@ -5,9 +5,6 @@ module Spree
     has_many :subscriptions, class_name: "Spree::Subscription", dependent: :restrict_with_error
 
     validates :title, presence: true
-    validates :label_status, presence: true, inclusion:
-      { in: Spree::Label.state_machine.states.map(&:name),
-        message: '%{value} is not a valid label status' }
     with_options allow_blank: true do
       validates :title, uniqueness: { case_sensitive: false }
     end
