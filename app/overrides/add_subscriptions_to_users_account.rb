2a02e4fb1f5782bd6406e26f1c9f2a783bfafdcb
Adam Foldvari <>
2020-02-25 14:05:04 +0100

    Delete my account view override

--- app/overrides/add_subscriptions_to_users_account.rb
+++
@@ -1,6 +0,0 @@
-Deface::Override.new(
-  virtual_path: 'spree/users/show',
-  name: 'add_subscriptions_to_users_account',
-  insert_before: '[data-hook="account_my_orders"]',
-  partial: 'spree/users/subscriptions'
-)
