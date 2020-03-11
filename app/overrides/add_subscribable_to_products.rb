f6c5fd5c5b33c5142dcd96547dadd141c156249f
Ivan Sheremeta <iisheremeta@gmail.com>
2020-03-11 14:23:16 +0200

    fix position of sbs checkbox in product edit

--- app/overrides/add_subscribable_to_products.rb
+++
@@ -1,6 +0,0 @@
-Deface::Override.new(
-  virtual_path: 'spree/admin/products/_form',
-  name: 'add_subscribable_to_product_edit',
-  insert_after: "[data-hook='admin_product_form_description']",
-  partial: 'spree/admin/products/subscribable',
-)
